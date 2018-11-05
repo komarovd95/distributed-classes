24fa0094036e1c5fb9ace0b13936be776873b78c
dmitry <dmitry@dmitry.komarov>
2018-11-06 01:18:00 +0300

    PA1 create ipc.c

--- pa1.c
+++ pa1.c
@@ -21,23 +21,24 @@ typedef struct {
 void create_pipes(int, FILE*, int**);
 
 void prepare_child_pipe(Pipe*, FILE*, int**);
-void execute_child(Pipe*);
 void cleanup_child_pipe(Pipe*, FILE*);
 
 void prepare_parrent_pipe(Pipe*, FILE*, int**);
-void execute_parrent(Pipe*);
 void cleanup_parrent_pipe(Pipe*, FILE*);
 
 void log_event(FILE*, const char*, ...);
 
-void send_started(Pipe*);
-void receive_started(Pipe*);
-
-void send_done(Pipe*);
-void receive_done(Pipe*);
-
 int serialize_msg(char*, const Message*);
 void deserialize_header(char*, MessageHeader*);
+
+void execute_child(Pipe*);
+void execute_parrent(Pipe*);
+
+void send_started(Pipe*, FILE*);
+void receive_started(Pipe*, FILE*);
+
+void send_done(Pipe*, FILE*);
+void receive_done(Pipe*, FILE*);
 
 int main(int argc, char *argv[]) {
     int p;
@@ -133,91 +134,6 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void create_pipes(int p, FILE *log, int **pipes) {
-    int i, j;
-
-    for (i = 1; i <= p; i++) {
-        for (j = 0; j <= p; j++) {
-            if (i != j) {
-                if (pipe(&pipes[i][j * 2]) == -1) {
-                    printf("Failed to create pipe: error=%s\n", strerror(errno));
-                    exit(1);
-                }
-                fprintf(log, "Create pipe: i=%d, j=%d, d=[%d, %d]\n", 
-                    i, j, pipes[i][j * 2], pipes[i][j * 2 + 1]);
-            }
-        }
-    }
-}
-
-void prepare_child_pipe(Pipe *pipe, FILE *log, int **pipes) {
-    int i, j;
-
-    for (i = 1; i <= pipe->total_p; i++) {
-        if (i != pipe->local_id) {
-            pipe->read_pipes[i] = pipes[i][pipe->local_id * 2];
-            close(pipes[i][pipe->local_id * 2 + 1]);
-            fprintf(log, "Closed pipe write endpoint (%d): from=%d to=%d\n", pipe->local_id, i, pipe->local_id);
-        }
-    }
-    for (i = 0; i <= pipe->total_p; i++) {
-        if (i != pipe->local_id) {
-            pipe->write_pipes[i] = pipes[pipe->local_id][i * 2 + 1];
-            close(pipes[pipe->local_id][i * 2]);
-            fprintf(log, "Closed pipe read endpoint (%d): from=%d to=%d\n", pipe->local_id, pipe->local_id, i);
-        }
-    }
-    for (i = 1; i <= pipe->total_p; i++) {
-        for (j = 0; j <= pipe->total_p; j++) {
-            if (i != j && i != pipe->local_id && j != pipe->local_id) {
-                close(pipes[i][j * 2]);
-                close(pipes[i][j * 2 + 1]);
-                fprintf(log, "Closed unused pipe (%d): from=%d to=%d\n", pipe->local_id, i, j);
-            }
-        }
-    }
-}
-
-void cleanup_child_pipe(Pipe *pipe, FILE *log) {
-    int i;
-
-    close(pipe->write_pipes[PARENT_ID]);
-    fprintf(log, "Clean up pipe (%d): from=%d to=%d\n", pipe->local_id, pipe->local_id, PARENT_ID);
-
-    for (i = 1; i <= pipe->total_p; i++) {
-        if (i != pipe->local_id) {
-            close(pipe->read_pipes[i]);
-            close(pipe->write_pipes[i]);
-            fprintf(log, "Clean up pipe (%d): from=%d to=%d\n", pipe->local_id, pipe->local_id, i);
-        }
-    }
-}
-
-void prepare_parrent_pipe(Pipe *pipe, FILE *log, int **pipes) {
-    int i, j;
-
-    for (i = 1; i <= pipe->total_p; i++) {
-        pipe->read_pipes[i] = pipes[i][0];
-
-        for (j = 1; j <= pipe->total_p; j++) {
-            if (i != j) {
-                close(pipes[i][j * 2]);
-                close(pipes[i][j * 2 + 1]);
-                fprintf(log, "Closed unused pipe (%d): from=%d to=%d\n", PARENT_ID, i, j);
-            }
-        }
-    }
-}
-
-void cleanup_parrent_pipe(Pipe *pipe, FILE *log) {
-    int i;
-
-    for (i = 1; i <= pipe->total_p; i++) {
-        close(pipe->read_pipes[i]);
-        fprintf(log, "Clean up pipe (%d): from=%d to=%d\n", PARENT_ID, i, PARENT_ID);
-    }
-}
-
 void execute_child(Pipe *pipe) {
     FILE *log;
 
@@ -227,19 +143,13 @@ void execute_child(Pipe *pipe) {
         exit(1);
     }
 
-    send_started(pipe);
-    log_event(log, log_started_fmt, pipe->local_id, getpid(), getppid());
-
-    receive_started(pipe);
-    log_event(log, log_received_all_started_fmt, pipe->local_id);
+    send_started(pipe, log);
+    receive_started(pipe, log);
 
     // Do some work
 
-    send_done(pipe);
-    log_event(log, log_done_fmt, pipe->local_id);
-
-    receive_done(pipe);
-    log_event(log, log_received_all_done_fmt, pipe->local_id);
+    send_done(pipe, log);
+    receive_done(pipe, log);
 
     fclose(log);
 }
@@ -253,28 +163,13 @@ void execute_parrent(Pipe *pipe) {
         exit(1);
     }
 
-    receive_started(pipe);
-    log_event(log, log_received_all_started_fmt, PARENT_ID);
-
-    receive_done(pipe);
-    log_event(log, log_received_all_done_fmt, PARENT_ID);
+    receive_started(pipe, log);
+    receive_done(pipe, log);
 
     fclose(log);
 }
 
-void log_event(FILE *log, const char *fmt, ...) {
-    va_list args;
-
-    va_start(args, fmt);
-    vfprintf(log, fmt, args);
-    va_end(args);
-    
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
-}
-
-void send_started(Pipe *pipe) {
+void send_started(Pipe *pipe, FILE *log) {
     Message *msg;
     MessageHeader msg_hdr;
     int payload_len;
@@ -292,11 +187,12 @@ void send_started(Pipe *pipe) {
     strcpy(msg->s_payload, buffer);
 
     send_multicast(pipe, msg);
+    log_event(log, buffer);
 
     free(msg);
 }
 
-void receive_started(Pipe *pipe) {
+void receive_started(Pipe *pipe, FILE *log) {
     int i;
 
     for (i = 1; i <= pipe->total_p; i++) {
@@ -312,9 +208,10 @@ void receive_started(Pipe *pipe) {
             free(msg);
         }
     }
+    log_event(log, log_received_all_started_fmt, pipe->local_id);
 }
 
-void send_done(Pipe *pipe) {
+void send_done(Pipe *pipe, FILE *log) {
     Message *msg;
     MessageHeader msg_hdr;
     int payload_len;
@@ -332,11 +229,12 @@ void send_done(Pipe *pipe) {
     strcpy(msg->s_payload, buffer);
 
     send_multicast(pipe, msg);
+    log_event(log, buffer);
 
     free(msg);
 }
 
-void receive_done(Pipe *pipe) {
+void receive_done(Pipe *pipe, FILE *log) {
     int i;
 
     for (i = 1; i <= pipe->total_p; i++) {
@@ -352,94 +250,5 @@ void receive_done(Pipe *pipe) {
             free(msg);
         }
     }
-}
-
-int send_multicast(void * self, const Message * msg) {
-    int p;
-    int i;
-    int local_id;
-    int serialized_size;
-    Pipe *pipe;
-    char buffer[MAX_MESSAGE_LEN];
-
-    pipe = (Pipe *) self;
-
-    p = pipe->total_p;
-    local_id = pipe->local_id;
-
-    serialized_size = serialize_msg(buffer, msg);
-    for (i = 0; i <= p; i++) {
-        if (local_id != i) {
-            if (write(pipe->write_pipes[i], buffer, serialized_size) != serialized_size) {
-                printf("Failed to write message to pipe: local_id=%d to=%d\n", local_id, i);
-                return 1;
-            }
-        }
-    }
-
-    return 0;
-}
-
-int receive(void * self, local_id from, Message * msg) {
-    Pipe *pipe;
-    char buffer[MAX_MESSAGE_LEN];
-
-    pipe = (Pipe *) self;
-
-    while (!read(pipe->read_pipes[from], buffer, sizeof(MessageHeader)));
-    deserialize_header(buffer, &msg->s_header);
-
-    while (!read(pipe->read_pipes[from], &buffer[sizeof(MessageHeader)], msg->s_header.s_payload_len));
-    strcpy(msg->s_payload, &buffer[sizeof(MessageHeader)]);
-
-    return 0;
-}
-
-int serialize_msg(char *buffer, const Message *message) {
-    int serialized_size;
-    MessageHeader header;
-
-    serialized_size = 0;
-
-    header = message->s_header;
-    buffer[serialized_size++] = header.s_magic >> 8;
-    buffer[serialized_size++] = header.s_magic;
-
-    buffer[serialized_size++] = header.s_payload_len >> 8;
-    buffer[serialized_size++] = header.s_payload_len;
-
-    buffer[serialized_size++] = header.s_type >> 8;
-    buffer[serialized_size++] = header.s_type;
-
-    buffer[serialized_size++] = header.s_local_time >> 8;
-    buffer[serialized_size++] = header.s_local_time;
-
-    strcpy(&buffer[serialized_size], message->s_payload);
-    serialized_size += header.s_payload_len;
-
-    return serialized_size;
-}
-
-void deserialize_header(char *buffer, MessageHeader *header) {
-    uint16_t magic;
-    uint16_t payload_len;
-    int16_t type;
-    timestamp_t local_time;
-
-    magic = buffer[0] << 8;
-    magic |= buffer[1];
-
-    payload_len = buffer[2] << 8;
-    payload_len |= buffer[3];
-
-    type = buffer[4] << 8;
-    type |= buffer[5];
-
-    local_time = buffer[6] << 8;
-    local_time = buffer[7];
-
-    header->s_magic = magic;
-    header->s_payload_len = payload_len;
-    header->s_type = type;
-    header->s_local_time = local_time;
+    log_event(log, log_received_all_done_fmt, pipe->local_id);
 }
